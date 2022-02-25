80f0ff46e16e97891ff5c906d904ca1852c248cf
asmit203 <asmit.ganguly@gmail.com>
2022-02-25 15:14:04 +0530

    0

--- CODEFORCES/0.cpp
+++ CODEFORCES/0.cpp
@@ -16,19 +16,33 @@ int main()
     {
         v.push_back(input[i]);
     }
-
-    cout << input[0];
-    for (int i = 0; i < v.size(); i++)
+    int leng = v.size();
+    int r = 0;
+    // cout << input[0];
+    for (int i = 0; i < leng; i++)
     {
         if (v[i] == 'a' || v[i] == 'e' || v[i] == 'i' || v[i] == 'o' || v[i] == 'u' || v[i] == 'A' || v[i] == 'E' || v[i] == 'I' || v[i] == 'O' || v[i] == 'U')
         {
             v.erase((v.begin() + i));
+            // cout<<"1\n";
+            r++;
+            // cout<<v[i]<<"\n";
         }
+
+        // cout<<"\\"<<v[i]<<"\n";
+    }
+    leng = leng - r;
+    cout<<leng<<"\n";
+    for (int i = 0; i < leng; i++)
+    {
+
+        v.insert((v.begin() + (2*i)), '.');
+        // printf("1\n");
     }
 
     for (int i = 0; i < v.size(); i++)
     {
-        cout << v[i];
+        cout << char(tolower((v[i])));
     }
 
     return 0;
